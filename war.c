6ca3af57aed1374281969d90ff7ab40867a2670b
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2026-03-27 18:26:16 +0000

    reeditei alguns pontos importates do código

--- war.c
+++ war.c
@@ -16,7 +16,7 @@ struct territorio{
 void limparBufferEntrada(){
     
     int c;
-    while((c = gertchar()) != '\n' && c != EOF);
+    while((c = getchar()) != '\n' && c != EOF);
 
 }
 
@@ -24,43 +24,48 @@ int main(){
 
     struct territorio mundo[mundo_max];
     int totalContinente = 0;
-    int i;
+    int i = 3;
+    int k = 1;
+    
+    printf("==============================\n\n" );
+    printf("Vamos cadastrar os 5 territórios iniciais do nosso mundo!\n\n");
+    printf("==============================\n\n" );
 
     //Laço do cadastro dos continentes
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 5; i++){
         
-        printf("Qual o continente?\n");
+        printf("--- Cadastrando o território %d\n", i + 1);
+
+        printf("Qual o nome? ");
         fgets(mundo[i].continente, tam_max, stdin);
         
-        printf("Qual a cor?\n");
+        printf("Qual a cor? ");
         fgets(mundo[i].cor, tam_max, stdin);
 
         mundo[i].continente[strcspn (mundo[i].continente, "\n")] = '\0';
-        mundo[i].cor[strcspn(mundo[i].cor, "\n")] = '\n';
+        mundo[i].cor[strcspn(mundo[i].cor, "\n")] = '\0';
 
-        printf("Quantas tropas?\n");
+        printf("Quantas tropas? ");
         scanf("%d", &mundo[i].tropa);
         limparBufferEntrada();
         
+        printf("\n");
     }
 
-    if(i != 0){
+        printf("\nCadastro inicial realizado com sucesso!\n\n");
+        printf("===============================\n");
+        printf("Mapa do mundo - ESTADO ATUAL\n");
+        printf("===============================\n\n");
         
-        for(int j = 0; j <= i; j++){
-            printf("Continente %d", j + 1);
-            printf("Nome: %s", mundo[j].continente);
-            printf("Cor: %s", mundo[j].cor);
-            printf("Tropa: %d", mundo[j].tropa);
+        for(int j = 0; j < 5; j++){
+            printf("TERRIÓRIO %d\n", j + 1);
+            printf("    -Nome: %s\n", mundo[j].continente);
+            printf("    -Cor: %s\n", mundo[j].cor);
+            printf("    -Tropa: %d\n", mundo[j].tropa);
         }
     }
 
 
-
-
-
-
-
-}
 
         
    
